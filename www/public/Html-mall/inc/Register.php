d9125735bedf95f082099177d8c6ff17440c337f
KarmaSora <karammtr@gmail.com>
2022-11-08 10:19:19 +0100

    added files/code from M2, need to add more

---
+++ www/public/Html-mall/inc/Register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="se">    
+<head>
+    <meta charset="UTF-8">
+    <title>Filhantering Skriv</title>
+</head>
+<body>
+
+<?php // filen motsvarat personWrite.php i M2
+    include("Person.php");
+    $personArray = array();
+
+    $newFirstName = $_POST['firstName'];
+    $newAfterName = $_POST['afterName'];
+    $newUser =  $_POST['Username'];
+    $newPwd = $_POST['pwd'];
+
+    $file = "person.dat";
+    if(file_exists($file)){
+        $personArray = unserialize(file_get_contents($file));
+        $personArray[] = new Person($newFirstName, $newAfterName, $newUser, $newPwd);
+        
+     }
+     file_put_contents($file, serialize($personArray));
+
+    // Färdigt! Resten av koden gör enbart en utskrift av innehållet.
+    header('Content-Type: text/html; charset=utf-8');   
+
+    for($i = 0; $i < count($personArray); $i++){
+      echo "<h1>" . $personArray[$i]->getFirstName() . " " . $personArray[$i]->getSurName() . "</h1>";
+      echo "<p><strong>Användarnamn: </strong>" . $personArray[$i]->getUserName() . "</p>";
+      echo "<p><strong>Lösenord: </strong>" . $personArray[$i]->getPassWord() . "</p>";
+      echo "<hr>";
+
+   }
+   
+
+?>
+
+<a href="loggIn.php">Go to login page!</a>
+</body>
+</html>
--- www/public/Html-mall/inc/dataControl.php
+++ www/public/Html-mall/inc/dataControl.php
@@ -1,16 +1,44 @@
 <?php
 session_start();
-if (isset($_POST['password'], $_POST['username'])) {
+// if (isset($_POST['password'], $_POST['username'])) {
 
-    if ($_POST['password'] == '12345' && $_POST['username'] == 'admin') {
-        $_SESSION['inLoggad'] = true;    // Användaren har anget rätt uppgifter.
-        $_SESSION['user'] = $_POST['username'];
+//     if ($_POST['password'] == '12345' && $_POST['username'] == 'admin') {
+//         $_SESSION['inLoggad'] = true;    // Användaren har anget rätt uppgifter.
+//         $_SESSION['user'] = $_POST['username'];
+//     }
+// }
+
+// // Om lyckad inloggning gå till index.php. Annars gå vidare och visa formuläret.
+// if (isset($_SESSION['inloggad'])) {
+//     header("Location: ../index.php");
+// } else {
+//     header("Location: ../index.php");
+// }
+    // För att förenkla användandet av mb_ funktioner
+    mb_internal_encoding("UTF-8");     
+    $file = "person.dat";
+    include("Person.php");
+
+if(!isset($_POST['user'])){
+    header("location: login.html");
+    exit;
+}
+
+    if(file_exists($file))
+    $personArray = unserialize(file_get_contents($file));
+
+
+    $user = $_POST['user'];
+    $pwd = $_POST['pwd'];
+
+
+
+for($i=0; $i<count($personArray); $i++){
+    if($user == $personArray[$i]->getUserName() && $pwd == $personArray[$i]->getPassWord()){
+        header("location: userPage.php" . "?name=" . $personArray[$i]->getUserName());
+        exit;
     }
 }
+header("location: login.html");
 
-// Om lyckad inloggning gå till index.php. Annars gå vidare och visa formuläret.
-if (isset($_SESSION['inloggad'])) {
-    header("Location: ../index.php");
-} else {
-    header("Location: ../index.php");
-}
+?>
